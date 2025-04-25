b283839d7ca8264c163e90a2f61467756975992d
SeongriPark <tjdfl0516@gmail.com>
2025-04-25 22:51:02 +0900

    firstcommit

--- src/main/java/com/poi/hr/controller/HomeController.java
+++ src/main/java/com/poi/hr/controller/HomeController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
 
     @GetMapping({"/", "/poihr"})
     public String home(Model model) {
         model.addAttribute("title", "Home");
+
+        // 현재 날짜와 시간
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd (E) HH:mm");
+        String formattedDateTime = now.format(formatter);
+        model.addAttribute("currentDateTime", formattedDateTime);
+
         return "index";
     }
-
-
 
 }
---
+++ src/main/java/com/poi/hr/dto/AttendanceDTO.java
@@ -0,0 +1,11 @@
+package com.poi.hr.dto;
+
+import java.time.LocalDateTime;
+
+public class AttendanceDTO {
+
+    private Integer attendId;
+    private LocalDateTime attendDate;
+    private LocalDateTime checkInDate;
+    private LocalDateTime checkOutDate;
+}
