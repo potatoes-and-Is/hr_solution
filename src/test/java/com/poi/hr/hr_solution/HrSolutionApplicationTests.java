84b7ecb7778c02c1f19ae689300244ff3f22c9fc
minyeong-lee <lmy1366@gmail.com>
2025-04-25 20:09:02 +0900

    first commit

---
+++ src/test/java/com/poi/hr/hr_solution/HrSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poi.hr.hr_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
