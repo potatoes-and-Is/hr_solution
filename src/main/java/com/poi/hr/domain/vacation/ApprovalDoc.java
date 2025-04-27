022208843353046928acc5ccdcef5bd1ffe5d6e4
minyeong-lee <lmy1366@gmail.com>
2025-04-27 18:16:52 +0900

    Entity 생성 - VacationReq

--- src/main/java/com/poi/hr/domain/vacation/ApprovalDoc.java
+++ src/main/java/com/poi/hr/domain/vacation/ApprovalDoc.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Approval_docs")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class ApprovalDoc {
 
     @Id
---
+++ src/main/java/com/poi/hr/domain/vacation/VacationReq.java
@@ -0,0 +1,73 @@
+package com.poi.hr.domain.vacation;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Vacation_reqs")
+public class VacationReq extends ApprovalDoc {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vac_type_id", nullable = false)
+    private VacationType vacationType;
+
+    @Column(name = "vac_req_start_date", nullable = false)
+    private LocalDate vacReqStartDate;
+
+    @Column(name = "vac_req_end_date", nullable = false)
+    private LocalDate vacReqEndDate;
+
+    @Column(name = "vac_use_days", nullable = false)
+    private int vacUseDays;
+
+    public VacationReq() {
+
+    }
+
+    public VacationReq(Employee employee, DocType docType, String approvalTitle, String approvalContent, String approvalReason, VacationType vacationType, LocalDate vacReqStartDate, LocalDate vacReqEndDate, int vacUseDays) {
+        super(employee, docType, approvalTitle, approvalContent, approvalReason);
+        this.vacationType = vacationType;
+        this.vacReqStartDate = vacReqStartDate;
+        this.vacReqEndDate = vacReqEndDate;
+        this.vacUseDays = vacUseDays;
+    }
+
+    public VacationType getVacationType() {
+        return vacationType;
+    }
+
+    public LocalDate getVacReqStartDate() {
+        return vacReqStartDate;
+    }
+
+    public LocalDate getVacReqEndDate() {
+        return vacReqEndDate;
+    }
+
+    public int getVacUseDays() {
+        return vacUseDays;
+    }
+
+    public void setVacReqStartDate(LocalDate vacReqStartDate) {
+        this.vacReqStartDate = vacReqStartDate;
+    }
+
+    public void setVacReqEndDate(LocalDate vacReqEndDate) {
+        this.vacReqEndDate = vacReqEndDate;
+    }
+
+    public void setVacUseDays(int vacUseDays) {
+        this.vacUseDays = vacUseDays;
+    }
+
+    @Override
+    public String toString() {
+        return "VacationReq{" +
+                "vacationTypeId=" + (vacationType != null ? vacationType.getVacTypeId() : null) +
+                ", vacReqStartDate=" + vacReqStartDate +
+                ", vacReqEndDate=" + vacReqEndDate +
+                ", vacUseDays=" + vacUseDays +
+                '}';
+    }
+}
