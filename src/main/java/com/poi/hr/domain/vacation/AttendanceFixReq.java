04ff4075e9232f2b656555e6832bb4c1dcb229f9
minyeong-lee <lmy1366@gmail.com>
2025-04-27 21:58:39 +0900

    Entity 생성 - ApprovalDoc의 자식들인 AttendanceFixReq, LeaveReq, RetireReq / SupportDoc 생성
    AttendanceFixReq 관계 ManyToOne으로 수정(ERD 수정)
    Unique 제약 조건 설정

---
+++ src/main/java/com/poi/hr/domain/vacation/AttendanceFixReq.java
@@ -0,0 +1,72 @@
+package com.poi.hr.domain.vacation;
+
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(
+        name = "Attendance_fix_reqs",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                    name = "uq_attendance_fix_req_attend_id",
+                    columnNames = {"attend_id"}
+                )
+        }
+)
+public class AttendanceFixReq extends ApprovalDoc {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attend_id")
+    private Attend attend;
+
+    @Column(name = "req_check_in_time")
+    private LocalTime reqCheckInTime;
+
+    @Column(name = "req_check_out_time")
+    private LocalTime reqCheckOutTime;
+
+    public AttendanceFixReq() {
+
+    }
+
+    public AttendanceFixReq(Employee employee, DocType docType, String approvalTitle, String approvalContent, String approvalReason, Attend attend, LocalTime reqCheckInTime, LocalTime reqCheckOutTime) {
+        super(employee, docType, approvalTitle, approvalContent, approvalReason);
+        this.attend = attend;
+        this.reqCheckInTime = reqCheckInTime;
+        this.reqCheckOutTime = reqCheckOutTime;
+    }
+
+    public Attend getAttend() {
+        return attend;
+    }
+
+    public LocalTime getReqCheckInTime() {
+        return reqCheckInTime;
+    }
+
+    public LocalTime getReqCheckOutTime() {
+        return reqCheckOutTime;
+    }
+
+    public void setAttend(Attend attend) {
+        this.attend = attend;
+    }
+
+    public void setReqCheckInTime(LocalTime reqCheckInTime) {
+        this.reqCheckInTime = reqCheckInTime;
+    }
+
+    public void setReqCheckOutTime(LocalTime reqCheckOutTime) {
+        this.reqCheckOutTime = reqCheckOutTime;
+    }
+
+    @Override
+    public String toString() {
+        return "AttendanceFixReq{" +
+                "attendId=" + (attend != null ? attend.getAttendId() : null) +
+                ", reqCheckInTime=" + reqCheckInTime +
+                ", reqCheckOutTime=" + reqCheckOutTime +
+                '}';
+    }
+}
