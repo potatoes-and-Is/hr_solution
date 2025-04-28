264b3c7b310679362688c67a6f22ea3d3aff5275
minyeong-lee <lmy1366@gmail.com>
2025-04-28 19:43:04 +0900

    [feat] 휴가 신청하기 모달 - 결재 등록 요청 보내기

--- src/main/java/com/poi/hr/controller/vacation/VacationApiController.java
+++ src/main/java/com/poi/hr/controller/vacation/VacationApiController.java
@@ -2,11 +2,11 @@ package com.poi.hr.controller.vacation;
 
 import com.poi.hr.dto.vacation.VacationTypeResDTO;
 import com.poi.hr.service.VacationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/poihr/api/vacation")
@@ -21,5 +21,13 @@ public class VacationApiController {
     @GetMapping("/types")
     public List<VacationTypeResDTO> getAllVacationTypes() {
         return vacationService.getAllVacationTypes();
+    }
+
+    @PostMapping("/vac-req/save")
+    public ResponseEntity<String> saveApprovalDocument(@RequestBody Map<String, Object> request) {
+
+        //결재 등록하기 함수 호출 필요
+
+        return ResponseEntity.ok("결재 문서 올리기 완료");
     }
 }
