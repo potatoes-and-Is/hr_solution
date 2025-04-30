01f82dbc1367ce9ddb81110e425ca7d02ac3ae97
KIM <emflazlwm62@gmail.com>
2025-04-30 17:45:39 +0900

    오류 수정

--- src/main/resources/static/js/button.js
+++ src/main/resources/static/js/button.js
@@ -10,9 +10,3 @@ document.addEventListener('click', function(e) {
         openCenteredPopup(`/author/employee/update/${employeeId}`, '직원 수정', 800, 600);
     }
 });
-// 직원 이름 클릭 시 상세 팝업 열기
-$(document).on('click', '.employee-detail-link', function (e) {
-    e.preventDefault();
-    const employeeId = $(this).data('id');
-    openCenteredPopup(`/author/employee/detail/${employeeId}`, '직원상세정보', 800, 500);
-});
