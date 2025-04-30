dfbb1faf9e2dc303d17b6685339f0ea30da06a5b
YoungJun <encode97@gmail.com>
2025-04-30 15:59:58 +0900

    approvallinehistoryrepository add code

--- src/main/java/com/poi/hr/repository/approval/ApprovalLineRepository.java
+++ src/main/java/com/poi/hr/repository/approval/ApprovalLineRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface ApprovalLineRepository extends JpaRepository<ApprovalLine, Integer> {
 
     // 문서 ID로 결재라인 리스트를 가져오되, 결재 순서로 정렬해서 가져온다
+    List<ApprovalLine> findByApprovalDocId(int approvalDocId);
     List<ApprovalLine> findByApprovalDoc_ApprovalDocIdOrderByApprovalLineOrderAsc(int approvalDocId);
 }
